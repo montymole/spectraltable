1d2d59cb458fd7f83b6f055eba91736c4d1b049a
Jussi Löf <jussi.lof@permobil.com>
2025-12-19 16:51:43 +0100

    advanced knob

--- src/main.ts
+++ src/main.ts
@@ -48,7 +48,6 @@ class SpectralTableApp {
     // Modulation Logic
     private lfos: LFO[] = [
         new LFO(0.5),
-        new LFO(0.5),
         new LFO(0.5)
     ];
     private pathYSource: string = 'none'; // 'none', 'lfo1', 'lfo2'
