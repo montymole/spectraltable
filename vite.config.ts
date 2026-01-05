b75f77753b90c42260753aa0c19f122f017dc6bc
Jussi Löf <jussi.lof@permobil.com>
2026-01-05 12:14:14 +0200

    merged

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: './',
     server: {
         port: 3000,
         open: true, // Auto-open browser on dev server start
     },
     build: {
-        outDir: 'dist',
+        outDir: 'docs',
+        assetsDir: '',
         sourcemap: true,
         target: 'es2020',
     },
